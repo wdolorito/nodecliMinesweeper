0e38ee30da776017f72d8b569e9d633db917647a
William Dolorito <wdolorito@gmail.com>
2019-12-17 14:13:33 -0800

    Create mine positions

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const readline = require('readline')
-let pickgame  = 'What game do you want to play?\n'
-    pickgame += '\t(N)ovice\n'
-    pickgame += '\t(I)ntermediate\n'
-    pickgame += '\t(E)xpert\n'
-    pickgame += '\t(Q)uit\n'
-
+const pickgame  = 'What game do you want to play?\n\t(N)ovice\n\t(I)ntermediate\n\t(E)xpert\n\t(Q)uit\n'
+const dprompt = 'pick a (row, column): '
+const ngame = '\nNovice game> ' + dprompt
+const igame = '\nIntermediate game> ' + dprompt
+const egame = '\nExpert game> ' + dprompt
+let currentgame
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -12,51 +12,81 @@ const rl = readline.createInterface({
   prompt: pickgame
 })
 
-rl.prompt()
+const createsolution = (rows, cols, mines) => {
+  const len = rows * cols
+  const sol = initgame(rows, cols, mines)
+  for(let count = 0; count < mines; count++) {
+    let mine = Math.floor(Math.random() * Math.floor(len))
+    if(sol[mine] == '.') {
+      sol[mine] = 'X'
+    } else {
+      count--
+    }
+  }
+  return sol
+}
+
+const initgame = (rows, cols, mines) => {
+  const len = rows * cols
+  const game = new Array(len)
+  game.fill('.', 0, len)
+  return game
+}
 
 const game = (newtype, newmines, newrows, newcols) => {
   return { name: newtype,
            mines: newmines,
            rows: newrows,
-           cols: newcols
+           cols: newcols,
+           solution: createsolution(newrows, newcols, newmines),
+           running: initgame(newrows, newcols, newmines)
          }
 }
 
-const novice = game('novice', 10, 9, 9)
-const intermediate = game('intermediate', 40, 16, 16)
-const expert = game('expert', 99, 16, 30)
-
 const drawBoard = (board) => {
+  const r = board.rows
+  const c = board.cols
+  const game = board.solution
+
   let row = ''
-  for(let count = 0; count < board.cols; count++) {
+  for(let count = 0; count < c; count++) {
     row += '\t' + count
   }
   console.log(row)
-  for(let rows = 1; rows < board.rows; rows++) {
+
+  for(let rows = 0; rows < r; rows++) {
     row = rows
-    for(let cols = 0; cols < board.cols; cols++) {
-      row += '\t' + cols
+    for(let cols = 0; cols < c; cols++) {
+      row += '\t' + game[(rows * c) + cols]
     }
     console.log(row)
   }
 }
 
+rl.prompt()
+
 rl.on('line', line => {
   switch (line.trim()) {
     case 'N':
     case 'n':
-      drawBoard(novice)
-      rl.setPrompt('Novice Game ')
+      currentgame = game('novice', 10, 9, 9)
+      drawBoard(currentgame)
+      rl.setPrompt(ngame)
+      console.time('game timer')
       break;
     case 'I':
     case 'i':
-      drawBoard(intermediate)
-      rl.setPrompt('Intermediate Game ')
+      currentgame = game('intermediate', 40, 16, 16)
+      drawBoard(currentgame)
+      rl.setPrompt(igame)
+      console.time('game timer')
       break;
     case 'E':
     case 'e':
-      drawBoard(expert)
-      rl.setPrompt('Expert Game ')
+      currentgame = game('expert', 99, 16, 30)
+      drawBoard(currentgame)
+      rl.setPrompt(egame)
+      console.time('game timer')
       break;
     case 'Q':
     case 'q':
